402c0167c8cf723b97112a2ea85089577e64b526
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-06-02 13:46:01 +0100

    FEATURE : Restore database from dump.

--- hitchbuildpg/database.py
+++ hitchbuildpg/database.py
@@ -2,6 +2,7 @@ from commandlib import CommandPath, Command
 from distutils.version import LooseVersion
 from hitchbuildpg import utils
 from path import Path
+from copy import copy
 import hitchbuild
 import signal
 import os
@@ -12,23 +13,43 @@ class PostgresDatabase(hitchbuild.HitchBuild):
         self.datafiles = self.as_dependency(datafiles)
         self.owner = self.as_dependency(owner)
         self._name = name
+        self._dump_filename = None
     
     def fingerprint(self):
         return (self._name,)
 
+    def from_dump(self, filename):
+        new_pgdb = copy(self)
+        new_pgdb._dump_filename = filename
+        return new_pgdb
+
+    @property
+    def psql(self):
+        return self.datafiles.psql(
+            "-U", self.owner.name, "-d", self._name,
+            "-p", "15432", "--host", self.datafiles.basepath
+        ).with_env(PG_PASSWORD=self.owner.password)
+
+    @property
+    def postgres(self):
+        return self.datafiles.postgres("-p", "15432")
+
     def build(self):
         server = self.datafiles.postgres("-p", "15432").pexpect()
         server.expect("database system is ready")
-        psql = self.datafiles.psql(
+        psql_superuser = self.datafiles.psql(
             "-d", "template1", "-p", "15432", "--host", self.datafiles.basepath,
         )
-        psql(
+        psql_superuser(
             "-c",
             "create database {} with owner {};".format(
                 self.name,
                 self.owner.name,
             )
         ).run()
+            
+        if self._dump_filename is not None:
+            self.psql("-f", self._dump_filename).run()
         
         os.kill(server.pid, signal.SIGTERM)
         server.close()
