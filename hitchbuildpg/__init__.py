760be4aeaffd94c940e48aa72f2415243effe480
Colm O'Connor <colm.oconnor.github@gmail.com>
2018-06-09 14:55:17 +0100

    REFACTOR : Removed old code.

--- hitchbuildpg/__init__.py
+++ hitchbuildpg/__init__.py
@@ -1,5 +1,3 @@
 from hitchbuildpg.app import PostgresApp
 from hitchbuildpg.datafiles import PostgresDatafiles
-from hitchbuildpg.database import PostgresDatabase
-from hitchbuildpg.user import PostgresUser
 from hitchbuildpg.datafiles import DataBuild
